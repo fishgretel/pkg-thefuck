ee75034f19fb27c6906b01f2ad800622201d6beb
Tom Hensel <github@jitter.eu>
2015-07-29 23:17:53 +0200

    initial commit https://github.com/nvbn/thefuck

---
+++ thefuck.fish
@@ -0,0 +1,21 @@
+# SYNOPSIS
+#   fuck
+#
+
+function init -a path --on-event init_thefuck
+  if not available thefuck
+    echo "💝  Please install 'thefuck' first!"
+  end
+end
+
+function fuck -d 'Correct your previous console command'
+    set -l exit_code $status
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    set -l fucked_up_commandd $history[1]
+    thefuck $fucked_up_commandd > $eval_script
+    . $eval_script
+    rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_commandd
+    end
+end
